5939ad22582ccee504ace675fb403b1c93702ff4
Артем <102693402+Clonn123@users.noreply.github.com>
2023-09-25 22:10:23 +0500

    Добавление первой и второй лабораторных работ

---
+++ 1_Work/main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import plotly as pl
+
+# Импортирование CSV
+df = pd.read_csv('DataSet1_1.csv', sep = ',')
+df.head()
+
+# Обращение к конкретной колонке данных
+
+# Обращение к конкретной ячейке данных
+
---
+++ 2_Work/main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_selection import VarianceThreshold
+import seaborn as sb
+
+# Импортирование CSV
+df = pd.read_csv('DataSet1_1.csv', sep = ',')
+df.head()
+
+# Обращение к конкретной колонке данных
+
+# Обращение к конкретной ячейке данных
+
